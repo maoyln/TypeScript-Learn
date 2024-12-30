7306a1926991aa2f904c034c06563094fbb7eeef
maoyln <maoyl_yx@163.com>
2024-12-30 17:05:22 +0800

    ts-之范型

--- leaning01-范型.ts
+++ leaning01-范型.ts
@@ -1,5 +1,5 @@
 // TypeScript 泛型中的 T、K、V 等到底是个啥？
-
+// https://juejin.cn/post/7095547569777934367?searchId=202412301344272392240D857FBF33BA65
 function identity<T, U>(value: T, message: U): T {
   console.log(message);
   return value;
