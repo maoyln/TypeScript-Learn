614b1dec279bb13b6d3293df4881b280efb555af
maoyln <maoyl_yx@163.com>
2024-12-30 17:22:41 +0800

    ts-联合类型

--- 02-联合类型.ts
+++ 02-联合类型.ts
@@ -1,5 +1,20 @@
+// 搞懂 TypeScript 联合类型的多个知识点！
+// https://juejin.cn/post/7095547569777934367?searchId=202412301344272392240D857FBF33BA65
+// Union Type
 function greet(person: string): string {
   return `Hello, ${person}`;
 }
 
 console.log(greet("world"));
+
+
+function greets(person: string | string[]): string | string[] {
+  if (Array.isArray(person)) {
+    return person.map((p) => `Hello, ${p}`)
+  } else if (typeof person === "string") {
+    return `Hello, ${person}`
+  }
+  throw new Error(`This should be impossible`);
+}
+
+console.log(greets(["a", "b", "c"]));
