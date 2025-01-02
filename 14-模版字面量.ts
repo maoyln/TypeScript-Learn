b5ed3dec1d302c997e1743ea5eba43f30955f68c
maoyln <maoyl_yx@163.com>
2025-01-02 16:46:48 +0800

    ts-字面量

--- 14-模版字面量.ts
+++ 14-模版字面量.ts
@@ -6,3 +6,6 @@ type ToString<T extends string | number | boolean | bigint> = `${T}`
 type T0 = EventName<'foo'>; // 'fooChanged'
 type T1 = Concat<'Hello', 'World'>; // 'Hello World'
 type T2 = ToString<'maoyl'| 888 | true | -1212n> // 'maoyl | '888' | 'true' | '1212n' 
+
+type T3 = ToString<'foo' | 'bar' | 'baz'>; // 'fooChanged' | 'barChanged' | 'bazChanged'
+type T4 = Concat<'top' | 'bottom', 'left' | 'right'>; // top-left | top-right | bottom-left | bottom-right
