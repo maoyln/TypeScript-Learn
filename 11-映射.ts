2d2c17441c5b890d376a5da8265cb8ac318cfa3b
maoyln <maoyl_yx@163.com>
2025-01-02 15:26:04 +0800

    ts-映射类型

--- 11-映射.ts
+++ 11-映射.ts
@@ -1,5 +1,21 @@
 // 10-工具类及实现 就是通过映射的形式进行操作的
 
+// 映射类型的语法
+// {readonly[P in K]?: T}
+
+/**
+ * 通过+号和-号表示添加或者移除修饰符
+ * 没有+、-则默认为+号
+ */
+
+// 所有映射类型语法
+// {[P in K]: T}
+// {[P in K]?: T}
+// {[P in K]-?: T}
+// { readonly [P in K]: T}
+// { readonly [P in K]?: T}
+// { -readonly [P in K]?: T}
+
 
 
 type Getters<T> = {
