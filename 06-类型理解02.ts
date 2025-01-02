9b1ad1afbb086a96dae317818712d3e2b973da3e
maoyln <maoyl_yx@163.com>
2025-01-02 10:22:02 +0800

    类型理解

--- 06-类型理解02.ts
+++ 06-类型理解02.ts
@@ -1,7 +1,25 @@
 // 索引（key）签名类型必须是：string、number、symbol类型或模版字面量类型
 // 值（value）的类型可以为任意类型
-interface TestKey {[key: string]: string}
+
+interface TypeValue {
+  id: string;
+  name: string;
+  age: number;
+
+}
+interface TestKey {[key: string | number | symbol]: TypeValue}
 
 interface Dictionary {
   // [key: boolean]: string; // 错误
+}
+
+
+interface PropChangeHandler {
+  [key: `${string}Changed`]: () => void
+}
+
+let handler: PropChangeHandler = {
+  idChanged: () => {}, // 对的
+  nameChanged: () => {}, // 对的
+  // ageChange: () => {} // 错的
 }
