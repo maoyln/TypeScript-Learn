4c11f6d49982d9742958116967ad0126af78628d
maoyln <maoyl_yx@163.com>
2025-01-03 13:59:03 +0800

    ts-练习2\3

---
+++ 20练习/203-联系3.ts
@@ -0,0 +1,28 @@
+interface Todo {
+  title: string;
+  description: string;
+}
+
+function updateTodo(todo: Todo, fieldsToUpdate: Partial1<Todo>) {
+  return { ...todo, ...fieldsToUpdate };
+}
+
+// lib.es5.d.ts
+type Partial1<T> = {
+  [P in keyof T]?: T[P];
+};
+
+type Foo = {
+  a: number;
+  b?: string;
+  c: boolean;
+ }
+ 
+ // 测试用例
+ type SomeOptional = SetOptional<Foo, 'a' | 'b'>;
+
+ type Simplify<T> = {
+  [P in keyof T]: T[P]
+}
+
+ type SetOptional<T, K extends keyof T> = Simplify<Partial<Pick<T, K>> & Pick<T, Exclude<keyof T, K>>>
