6e01d72d3f6e398a2e0de80692ab869a59ce554e
maoyln <maoyl_yx@163.com>
2025-01-13 13:44:59 +0800

    泛型

--- 01-范型.ts
+++ 01-范型.ts
@@ -1,13 +1,13 @@
 // TypeScript 泛型中的 T、K、V 等到底是个啥？
 // https://juejin.cn/post/7095547569777934367?searchId=202412301344272392240D857FBF33BA65
 function identity<T, U>(value: T, message: U): T {
-  console.log(message);
+  console.log(`message->${message}`);
   return value;
 }
 
-const a = identity<string, number>("hello", 1);
-const a1 = identity("hello", 1);
+const a = identity<string, number>("hello world", 1);
+const a1 = identity("hello myl", 2);
 
-console.log(a);
-console.log(a1);
+console.log(`a->${a}`);
+console.log(`a1->${a1}`);
 
